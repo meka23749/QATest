506583e4519ef717cbde03b6807d24456eff63a8
meka23749 <meka23749@gmail.com>
2026-02-06 21:27:16 +0100

    add simple HTTP healthcheck server for testing

---
+++ server.py
@@ -0,0 +1,17 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/health":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+if __name__ == "__main__":
+    server = HTTPServer(("localhost", 8000), Handler)
+    print("Server running on http://localhost:8000")
+    server.serve_forever()
